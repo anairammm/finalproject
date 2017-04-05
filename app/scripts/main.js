71e8e3f97e9570eff273dab0e941dac9309e1365
MaRiAnA <jeanmariananuman@muhMac.local>
2017-04-05 16:53:10 -0400

    Couple sections set up

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,18 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		responsiveWidth: 900,
+		menu: '#menu',
+		anchors: ['firstPage', 'secondPage', '3rdPage'],
+		sectionsColor: ['#fff', '#818181', '#fff'],
+		hybrid:true,
+    	fitToSection: false,
+		autoScrolling: false,
+		afterLoad: function(anchorLink, index){	
+		//setTimeout(function () {
+			//$('.head').show().addClass('animated fadeInRight');}, 600
+			//);
+		},
+		onLeave: function(index, nextIndex, direction){
+		},
+	});
+});
