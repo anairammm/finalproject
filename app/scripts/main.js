f00b52f109912eecbe58679edcdcdb18f44a9ba7
MaRiAnA <jeanmariananuman@muhMac.local>
2017-04-23 22:13:12 -0400

    animations working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,20 +2,22 @@ $(document).ready(function() {
 	$('#fullpage').fullpage({
 		responsiveWidth: 900,
 		//menu: '#menu',
-		anchors: ['firstPage', 'secondPage', '3rdPage'],
-		sectionsColor: ['#4E4E4E', '#818181', '#4E4E4E', '#818181', '#4E4E4E', '#818181', '#4E4E4E'],
+		anchors: ['firstPage', 'secondPage', '3rdPage', 'fourthPage', '5thPage', 'sixthPage', '7thPage'],
+		sectionsColor: ['#4E4E4E', '#818181', '#4E4E4E', '#818181', '#4E4E4E', '#818181', '#4E4E4E', '#818181'],
 		navigation: true,
 		navigationPosition: 'left',
 		hybrid:true,
     	fitToSection: false,
 		autoScrolling: false,
 		afterLoad: function(anchorLink, index){	
-		//setTimeout(function () {
-			//$('.head').show().addClass('animated fadeInRight');}, 600
-			//);
+			setTimeout(function () {
+				$('svg').show().addClass('animated fadeInRight');}, 800
+				);
 		},
 		onLeave: function(index, nextIndex, direction){
+			$('svg').hide();
 		},
+		//},
 	
 		
 	});
